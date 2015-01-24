24ab38b8e37001150e34fa8b798a08fa2a409559
irfanr <irfan.romadona@gmail.com>
2015-01-25 01:55:08 +0700

    initial commit

---
+++ src/main/java/com/mascova/caliga/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.mascova.caliga.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
